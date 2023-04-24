9c3086f2a803fbf199bcba64c135a77063ffdc67
Marwan <102687529+Maroibo@users.noreply.github.com>
2023-04-24 19:02:24 +0300

    created an api to get all the conferences

---
+++ APIs/conf-plus/app/api/conference/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import * as repo from "../repository.js";
+export async function GET(request) {
+  try {
+    const response = await repo.readAllConferences();
+    if (response.conferences.length > 0) {
+      return NextResponse.json(response.conference);
+    } else {
+      return NextResponse.json({
+        error: "There are no conferences",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
